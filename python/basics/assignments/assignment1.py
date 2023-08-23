98ac83fd49fc339b12f7a39a7fc8c05226a52612
ravivelamala <ravivelamala.marolix@gmail.com>
2023-08-23 09:51:36 +0530

    assignment 1

---
+++ python/basics/assignments/assignment1.py
@@ -0,0 +1,13 @@
+# membership operator using in and not in 
+a = "ravi"
+string_a = "hi ravi"
+if (a in string_a):
+    print("True")
+else:
+    print("False") # using in operator
+a = "ravi"
+string_a = "hi ravi"
+if (a not in string_a):
+    print("True")
+else:
+    print("False")
